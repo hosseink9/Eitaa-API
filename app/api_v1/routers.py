f057602d0d4f041454482196006172b5f69900b3
hosseink9 <hosseinkalantari.29@gmail.com>
2024-08-04 16:06:20 +0330

    Fix code for send more than 1 file

--- app/api_v1/routers.py
+++ app/api_v1/routers.py
@@ -5,17 +5,20 @@ from fastapi.routing import APIRouter
 router = APIRouter(tags=["Eitaa"])
 
 
-@router.post('/send-data', status_code=status.HTTP_200_OK)
-async def create_post(file: UploadFile = File(...), token: str = Form(...), chat_id: str = Form(...), caption: str = Form(None)):
+@router.post('/send-data', status_code=status.HTTP_200_OK, description="You can use this function for create post in Eitta chanel or group")
+async def create_post(files: list[UploadFile] = File(...), token: str = Form(...), chat_id: str = Form(...), caption: str = Form(None)):
     url = f"https://eitaayar.ir/api/{token}/sendFile"
     send_data = {
         "chat_id": chat_id,
         "caption": caption
     }
-    async with httpx.AsyncClient() as client:
-        response = await client.post(url, data=send_data, files={"file": file.file})
-        if response.status_code == 200:
-            return response.json()
-        else:
-            raise HTTPException(status_code=response.status_code, detail=response.text)
+    for file in files:
+        async with httpx.AsyncClient() as client:
+            response = await client.post(url, data=send_data, files={"file": file.file})
+            response_list =[]
+            if response.status_code == 200:
+                response_list.append(response.json())
+            else:
+                raise HTTPException(status_code=response.status_code, detail=response.text)
+    return response_list
 
